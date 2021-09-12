3ece9840c82c13ad11a590e14a22ec515e6e12af
CodeForFunUIT <nghiathdev@gmail.com>
2021-09-12 17:58:28 +0700

    xxx
    cvcv

---
+++ lib/screen/splash/intro/intro.dart
@@ -0,0 +1,115 @@
+import 'dart:math';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/main.dart';
+// import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> with TickerProviderStateMixin {
+  late AnimationController _lottieAnimation;
+  var expanded = false;
+  double _bigFontSize = kIsWeb ? 234 : 178;
+  final transitionDuration = Duration(seconds: 1);
+
+  @override
+  void initState() {
+    _lottieAnimation = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    Future.delayed(Duration(seconds: 1))
+        .then((value) => setState(() => expanded = true))
+        .then((value) => Duration(seconds: 1))
+        .then(
+          (value) => Future.delayed(Duration(seconds: 1)).then(
+            (value) => _lottieAnimation.forward().then(
+                  (value) => Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              MyHomePage(title: 'Flutter Demo Home Page')),
+                      (route) => false),
+                ),
+          ),
+        );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.grey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedDefaultTextStyle(
+              duration: transitionDuration,
+              curve: Curves.fastOutSlowIn,
+              style: TextStyle(
+                color: Color(0xFF4e954e),
+                fontSize: !expanded ? _bigFontSize : 50,
+                fontFamily: 'Montserrat',
+                fontWeight: FontWeight.w600,
+              ),
+              child: Text(
+                "N",
+              ),
+            ),
+            AnimatedCrossFade(
+              firstCurve: Curves.fastOutSlowIn,
+              crossFadeState: !expanded
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: transitionDuration,
+              firstChild: Container(),
+              secondChild: _logoRemainder(),
+              alignment: Alignment.bottomCenter,
+              sizeCurve: Curves.easeInOut,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _logoRemainder() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "GHIA",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 50,
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 30.0),
+          child: LottieBuilder.asset(
+            'assets/food.json',
+            onLoaded: (composition) {
+              _lottieAnimation..duration = composition.duration;
+            },
+            frameRate: FrameRate.max,
+            repeat: false,
+            animate: false,
+            height: 90,
+            width: 90,
+            controller: _lottieAnimation,
+          ),
+        )
+      ],
+    );
+  }
+}
